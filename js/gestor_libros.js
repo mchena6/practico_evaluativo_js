40fa9d85bef770e4b5d662d0099b05618dff3ac2
mchena <m.chena@itecriocuarto.org.ar>
2025-06-29 20:59:14 -0300

    Funcion filtrar leidos agregada y funcion resumen actualizada

--- js/gestor_libros.js
+++ js/gestor_libros.js
@@ -1,6 +1,5 @@
 
 // Variables para manejar estados
-let editando = false;
 let indice_edicion = null;
 let orden_ascendente = false;
 
@@ -15,11 +14,13 @@ const seleccion_generos = (id) =>{
     const select = document.getElementById(id)
     select.innerHTML = ''
 
-    // Opción "Todos"
-    const todos = document.createElement('option')
-    todos.value = 'todos'
-    todos.textContent = '-- Todos los géneros --'
-    select.appendChild(todos)
+    // Agregar opcion todos al utilizar el filtrado por genero
+    if (id === 'busqueda_genero') {
+        const todos = document.createElement('option')
+        todos.value = 'todos'
+        todos.textContent = '-- Todos los géneros --'
+        select.appendChild(todos)
+    }
     
     generos.forEach( genero =>{
         const option = document.createElement('option')
@@ -35,33 +36,36 @@ const agregar_libro = () =>{
     // Guardar valores del formulario 
     const titulo = document.getElementById('titulo').value.trim()
     const autor = document.getElementById('autor').value.trim()
-    const anio = document.getElementById('anio').value.trim()
+    const anio = parseInt(document.getElementById('anio').value.trim())
     const genero = document.getElementById('genero').value
+    const leido = document.getElementById('leido').checked
 
     // Comprobar que no esten vacios
-    if (titulo !== '' && autor !== '' && anio !== ''){
+    if (titulo !== '' && autor !== '' && anio !== '' && anio > 1900 && anio < 2026){
         // Comprobar si se esta editando o agregando un libro nuevo
         if(indice_edicion !== null){
             // Guardar los nuevos valores del libro
-            libros[indice_edicion] = {titulo, autor, anio, genero}
+            libros[indice_edicion] = {titulo, autor, anio, genero, leido}
+            // Cambiar texto del boton
+            document.getElementById('button_form').textContent = 'Agregar libro'
             // Vaciar indice de edicion
             indice_edicion = null
-            // Cambiar texto del boton
-            document.getElementById('button_form').textContent = 'Guardar cambios'
         }
         else{
         // Agregar libro a la lista
-        libros.push({ titulo, autor, anio, genero })
+        libros.push({ titulo, autor, anio, genero, leido})
     }
     // Guardar lista de la libros en local storage
     localStorage.setItem('libros', JSON.stringify(libros))} 
     // Mostrar libros
     renderizar_libros()
+    mostrar_resumen()
 
     // Vaciar los campos del formulario
     document.getElementById('titulo').value = ''
     document.getElementById('autor').value = ''
     document.getElementById('anio').value = ''
+    document.getElementById('leido').checked = false 
 }
 
 
@@ -73,8 +77,10 @@ const eliminar_libro = (index) =>{
     // Actualizar local storage y renderizar libros
     localStorage.setItem('libros', JSON.stringify(libros))
     renderizar_libros()
+    mostrar_resumen()
 }
 
+// Editar libro
 const editar_libro = (index) =>{
     // Identificar libro usando el indice
     const libro = libros[index]
@@ -82,10 +88,14 @@ const editar_libro = (index) =>{
     document.getElementById('autor').value = libro.autor
     document.getElementById('anio').value = libro.anio
     document.getElementById('genero').value = libro.genero
+    document.getElementById('leido').checked = libro.leido
 
     // Guardar indice y cambiar valor de variable 
     indice_edicion = index
-    editando = true
+    document.getElementById('button_form').textContent = 'Guardar cambios'
+
+    // Actualizar el resumen
+    mostrar_resumen()
 }
 
 // Filtrar libros por titulo
@@ -118,6 +128,33 @@ const buscar_genero = () =>{
     // Mostrar libros filtrados
     renderizar_libros(generos_filtrados)
 }
+
+const filtrar_leidos = () =>{
+
+    // Guardar opcion elegida 
+    const opcion_seleccionada = document.getElementById('filtrar_leidos').value 
+    
+    // Lista para guardar libros filtrados
+    let libros_filtrados = []
+    
+    // Filtrar libros segun opcion elegida
+    if (opcion_seleccionada === 'leidos'){
+        // Guardar libros leidos
+        libros_filtrados = libros.filter(libro => libro.leido)
+    }
+    else if (opcion_seleccionada === 'no_leidos'){
+        // Guardar libros no leidos
+        libros_filtrados = libros.filter(libro => !libro.leido)
+    }
+    else{
+        // Guardar todos los libros
+        libros_filtrados = libros
+    }
+
+    // Mostrar libros filtrados
+    renderizar_libros(libros_filtrados)
+}
+
 
 const ordenar_libros = () =>{
     
@@ -156,6 +193,7 @@ const renderizar_libros = (lista = libros) =>{ // Recibe una lista
             <td>${libro.autor}</td>
             <td>${libro.anio}</td>
             <td>${libro.genero}</td>
+            <td>${libro.leido ? 'Leido' : 'No leido'}</td>
             <td>
                 <button onclick="editar_libro(${index})">Editar</button>
                 <button onclick="eliminar_libro(${index})">Eliminar</button>
@@ -171,7 +209,8 @@ const mostrar_resumen = () =>{
     
     const resumen = document.getElementById('resumen_libros')
     
-    if (libros.lenght === 0){
+    // Verificar que haya libros en la lista
+    if (libros.length === 0){
         resumen.innerText = 'No se muestran datos cargados'
         return;
     }   
@@ -182,7 +221,7 @@ const mostrar_resumen = () =>{
     // Total de años
     const suma_anios = libros.reduce((contador, libro) => contador + parseInt(libro.anio), 0)
     
-    // Promedio de años
+    // Promedio de año de publicacion
     const promedio_anio = Math.round(suma_anios/total_libros)
 
     // Libros posteriores al 2010
@@ -194,20 +233,29 @@ const mostrar_resumen = () =>{
     // Libro mas antiguo
     const mas_antiguo = libros.reduce((antiguo, libro) => (libro.anio < antiguo.anio ? libro : antiguo), libros[0])
 
+    // Cantidad de libros leidos
+    const libros_leidos = libros.filter(libro => libro.leido).length
+    
+    // Cantidad de libros no leidos
+    const libros_no_leidos = total_libros - libros_leidos    
+
+    // Mostrar resumen de datos
     resumen.innerHTML = `
         <p> Total de libros: ${total_libros}</p>
         <p> Promedio de años: ${promedio_anio}</p>
         <p> Libros posteriores al año 2010: ${libros_post_2010}</p>
         <p> Libro mas reciente: ${mas_reciente.titulo} ${mas_reciente.anio}</p>
         <p> Libro mas antiguo: ${mas_antiguo.titulo} ${mas_antiguo.anio}</p>
-    `
+        <p> Total de libros leidos: ${libros_leidos}
+        <p> Total de libros por leer: ${libros_no_leidos}
+        `
 }
 
-
-
+// Funciones que se ejecutan al cargar la pagina
 document.addEventListener('DOMContentLoaded', () => {
     renderizar_libros()
     seleccion_generos('genero')
     seleccion_generos('busqueda_genero')
+    filtrar_leidos()
     mostrar_resumen()
 })
